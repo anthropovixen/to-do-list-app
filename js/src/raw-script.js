f14d55835d3744688e0f3579d565ea46e7967a35
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-01-26 21:49:15 +1100

    Update raw-script.js
    Change format with Prettier

--- js/src/raw-script.js
+++ js/src/raw-script.js
@@ -1,33 +1,40 @@
-function newItem(){
+function newItem() {
+	let li = $('<li></li>');
+	let inputValue = $('#input').val();
+	li.append(inputValue);
 
-    let li = $('<li></li>');    
-    let inputValue = $('#input').val();
-    li.append(inputValue);
-  
-    if ($('#input').val() === '') {
-      alert('You must write something!');
-      } else {
-      $('#list').append(li);
-    }
-  
-    function crossOut() {
-      li.toggleClass('strike');
-    }
-  
-    li.on('dblclick', function crossOut() {
-      li.toggleClass('strike');
-      });
-  
-    let crossOutButton = $('<crossOutButton></crossOutButton>');
-    crossOutButton.append(document.createTextNode('X'));
-    li.append(crossOutButton);
-  
-    crossOutButton.on('click', deleteListItem);
-  
-    function deleteListItem () {
-      li.addClass('delete')
-    }
-  
-    $('#list').sortable();
-   
-  }
+	if ($('#input').val() === ' ') {
+		alert('You must write something!');
+		e.preventDefault();
+	} else {
+		$('#list').append(li);
+	}
+
+	$('#input').keydown(function (e) {
+		if (e.which === 13) {
+			e.preventDefault();
+			$('li').append(inputValue);
+			this.value = '';
+		}
+	});
+
+	function crossOut() {
+		li.toggleClass('strike');
+	}
+
+	li.on('dblclick', function crossOut() {
+		li.toggleClass('strike');
+	});
+
+	let crossOutButton = $('<crossOutButton></crossOutButton>');
+	crossOutButton.append(document.createTextNode('X'));
+	li.append(crossOutButton);
+
+	crossOutButton.on('click', deleteListItem);
+
+	function deleteListItem() {
+		li.addClass('delete');
+	}
+
+	$('#list').sortable();
+}
