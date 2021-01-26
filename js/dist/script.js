39a289cfe825d97dfad91199ac1fd1ca70a0ab2a
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-01-26 21:59:16 +1100

    Update script.js
    Minified file changed

--- js/dist/script.js
+++ js/dist/script.js
@@ -1,1 +1,22 @@
-function newItem(){let t=$("<li></li>"),e=$("#input").val();t.append(e),""===$("#input").val()?alert("You must write something!"):$("#list").append(t),t.on("dblclick",function(){t.toggleClass("strike")});let n=$("<crossOutButton></crossOutButton>");n.append(document.createTextNode("X")),t.append(n),n.on("click",function(){t.addClass("delete")}),$("#list").sortable()}
+function newItem() {
+	let t = $('<li></li>'),
+		n = $('#input').val();
+	t.append(n),
+		' ' === $('#input').val()
+			? (alert('You must write something!'), e.preventDefault())
+			: $('#list').append(t),
+		$('#input').keydown(function (t) {
+			13 === t.which &&
+				(t.preventDefault(), $('li').append(n), (this.value = ''));
+		}),
+		t.on('dblclick', function () {
+			t.toggleClass('strike');
+		});
+	let l = $('<crossOutButton></crossOutButton>');
+	l.append(document.createTextNode('X')),
+		t.append(l),
+		l.on('click', function () {
+			t.addClass('delete');
+		}),
+		$('#list').sortable();
+}
